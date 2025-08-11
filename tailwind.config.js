b7a37030fdd91afb261ac85f1801b01fbcfafc4b
Mark Burrell <mburrell@biotrust.com>
2025-08-11 11:10:24 -0600

    Move theme files into src directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './layout/*.liquid',
-    './templates/*.liquid',
-    './templates/**/*.liquid',
-    './sections/*.liquid',
-    './snippets/*.liquid',
-    './blocks/*.liquid',
-    './assets/*.js'
+    "./src/layout/*.liquid",
+    "./src/templates/*.liquid",
+    "./src/templates/**/*.liquid",
+    "./src/sections/*.liquid",
+    "./src/snippets/*.liquid",
+    "./src/blocks/*.liquid",
+    "./src/assets/*.js",
   ],
   theme: {
     extend: {
       animation: {
-        'marquee': 'marquee var(--tw-animate-duration, 30s) linear infinite',
-        'marquee-duplicate': 'marquee-duplicate var(--tw-animate-duration, 30s) linear infinite',
+        marquee: "marquee var(--tw-animate-duration, 30s) linear infinite",
+        "marquee-duplicate":
+          "marquee-duplicate var(--tw-animate-duration, 30s) linear infinite",
       },
       keyframes: {
         marquee: {
-          '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(-100%)' },
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-100%)" },
         },
-        'marquee-duplicate': {
-          '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(-100%)' },
+        "marquee-duplicate": {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-100%)" },
         },
       },
     },
   },
   plugins: [],
-}
+};

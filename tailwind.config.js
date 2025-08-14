57c15d06cb9805ddea09ad07e66b7f16d8ef307f
Ryan Elledge <relledge@wellbeam.com>
2025-08-14 16:03:29 -0500

    updating the blue to the bt blue

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,21 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        blue: {
+          50: '#eff6ff',
+          100: '#dbeafe',
+          200: '#bfdbfe',
+          300: '#93c5fd',
+          400: '#60a5fa',
+          500: '#3b82f6',
+          600: '#0D4F9A',
+          700: '#1d4ed8',
+          800: '#1e40af',
+          900: '#1e3a8a',
+          950: '#172554',
+        },
+      },
       animation: {
         marquee: "marquee var(--tw-animate-duration, 30s) linear infinite",
         "marquee-duplicate":
